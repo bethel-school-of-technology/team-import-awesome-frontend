1abd8d0d7145f4197d8f8475897282ea9fee718e
Aaron Partridge <apartridge16@gmail.com>
2023-04-26 16:22:22 -0700

    navbar working / added global css and navbar css / fixed refresh issue in SignIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,38 +1,51 @@
 import React, { useContext, useState } from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import UserContext from '../contexts/userContext.js';
 
-
 const SignIn = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     let { loginUser } = useContext(UserContext);
     let navigate = useNavigate();
 
     function handleSubmit(event) {
         event.preventDefault();
-        loginUser(username, password).then(() => {
-            navigate('/');
-        }).catch(error => {
-            console.log(error);
-            window.alert('Failed login');
-        });
+        loginUser(username, password)
+            .then(() => {
+                navigate('/');
+                window.location.reload(true);
+            })
+            .catch((error) => {
+                console.log(error);
+                window.alert('Failed login');
+            });
     }
 
     return (
         <form onSubmit={handleSubmit} className="text-center">
             <br></br>
-                <div className="mt-20">
-                    Welcome Back!
-                </div>
+            <div className="mt-20">Welcome Back!</div>
 
             <br></br>
-            <input className="p-2 border-[1px] ml-2 rounded-md border-gray-600" placeholder="Username" type="text" name="userName" onChange={e => setUsername(e.target.value)} />
-            <br></br><br></br>
-            <input className="p-2 border-[1px] ml-2 rounded-md border-gray-600"  placeholder="Password" type="password" name="password" onChange={e => setPassword(e.target.value)} />
-            <br/>
-            <br/>
+            <input
+                className="p-2 border-[1px] ml-2 rounded-md border-gray-600"
+                placeholder="Username"
+                type="text"
+                name="userName"
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <br></br>
+            <br></br>
+            <input
+                className="p-2 border-[1px] ml-2 rounded-md border-gray-600"
+                placeholder="Password"
+                type="password"
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <br />
             <button className="bg-[#FD358E] hover:bg-pink-400 text-white font-bold py-2 px-4 border border-bg-[#FD358E] rounded">
                 Sign In
             </button>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import React from 'react';
-
+import './css/App.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-
     <App />
-//followed the directions on an error message to fix create react app
-//said react no longer supported and needed to move to createRoot
-
-)
+    //followed the directions on an error message to fix create react app
+    //said react no longer supported and needed to move to createRoot
+);
